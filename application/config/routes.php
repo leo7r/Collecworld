e765e853efc77f48a145ccb4cb65f319dd446e6a
leo7r <ramosleo12@gmail.com>
2014-05-12 19:26:05 -0430

    Organizados las rutas a los controladores

--- application/config/routes.php
+++ application/config/routes.php
@@ -76,168 +76,108 @@
 
 */
 
+/* Seccion general de Collecworld */
 $route['default_controller'] = "welcome/view";
-
 $route['404_override'] = "welcome/view";
-
-$route['out'] = "init/logout";
-
-$route['init'] = "init/view";
-
-$route['explore'] = "explore/phonecard";
-
-$route['test'] = "init/test";
-
 $route['change_language'] = "init/change_language";
 
+/* Iniciar y Cerrar sesion */
+$route['login'] = "login/view";
+$route['out'] = "init/logout";
 
+/* Seccion Inicio y Test */
+$route['init'] = "init/view";
+$route['test'] = "init/test";
 
-//help
-
+/* Seccion de ayuda */
 $route['help'] = "help";
-
 $route['help/get_started'] = "help/get_started";
-
 $route['help/upload/phonecards'] = "help/phonecards";
-
 $route['help/account'] = "help/account";
-
 $route['help/tools'] = "help/tools";
-
 $route['help/collecworld_community'] = "help/collecworld_community";
 
-
-
-$route['export/(pdf|excel)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)'] = "export/view/$1/$2/$3/$4/$5/$6/$7/$8/$9";
-
-$route['explore/(phonecard|coin|banknote)'] = "explore/$1";
-
-$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7";
-
-$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/no_variations'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/0/1";
-
-$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/(by_catalog|by_reference|by_face_value|by_serie)'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/$8";
-
-$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/(by_catalog|by_reference|by_face_value|by_serie)/no_variations'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/$8/1";
-
-$route['explore/coin/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/([a-z0-9]+)/(:num)'] = "explore/explore_coin/$1/$2/$3/$4/$5/$6/$7/$8/$9";
-
-$route['explore/coin/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/([a-z0-9]+)/(:num)/no_variations'] = "explore/explore_coin/$1/$2/$3/$4/$5/$6/$7/$8/0/1";
-
-$route['explore/banknote/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/(:num)'] = "explore/explore_banknote/$1/$2/$3/$4/$5/$6/$7/$8";
-
-$route['explore/banknote/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/(:num)/no_variations'] = "explore/explore_banknote/$1/$2/$3/$4/$5/$6/$7/0/1";
-
-
-
-
-$route['event/create'] = "event/new_event";
-
-$route['event/create_go'] = "event/create";
-
-$route['event/edit/(:num)'] = "event/edit/$1";
-
-$route['event/edit/edit_go'] = "event/edit_go";
-
-$route['event/(:num)(-[a-zA-Z0-9]*)?'] = "event/view/$1";
-
-$route['event/(:num)(-[a-zA-Z0-9]*)?/invite'] = "event/invite/$1";
-
-$route['event/events_list'] = "event/events_list";
-
-
-
-/* Trade */
-
-
-
-$route['trade/([A-Za-z0-9]+)'] = "trade/select_only_user/$1";
-
-$route['trade/(phonecard|coin|banknote)/(:num)'] = "trade/select/$1/$2";
-
-$route['trade/exchange/([A-Za-z0-9]+)/step_2'] = "trade/exchange_step2/$1";
-
-$route['trade/(buy|exchange)/([A-Za-z0-9]+)'] = "trade/create_only_user/$1/$2";
-
-$route['trade/(buy|exchange)/(phonecard|coin|banknote)/(:num)'] = "trade/create/$1/$2/$3";
-
-$route['trade/(buy|exchange)/(phonecard|coin|banknote)/(:num)/(:num)/([A-Za-z0-9]+)'] = "trade/create/$1/$2/$3/$4/$5";
-
-$route['trade/trade_buy'] = "trade/trade_buy";
-
-$route['trade/trade_exchange'] = "trade/trade_exchange";
-
-$route['trade/trade_details/(:num)'] = "trade/trade_details/$1";
-
-
-
-$route['login'] = "login/view";
-
+/* Seccion subir */
 $route['upload'] = "upload/view";
-
-$route['account'] = "user/viewAccount";
-
-$route['forgot_password'] = "user/forgot_password";
-$route['reset_password/(:num)/(:any)'] = "user/reset_password/$1/$2";
-
-$route['edit_user'] = "user/edit_user";
-
-$route['signup'] = "user/new_user";
-
-$route['signin'] = "user/login";
-
-$route['activate_user/(:num)/(:any)'] = "user/activate_user/$1/$2";
-
 $route['upload/restriction'] = "upload/restriction";
-
 $route['upload/upload_go'] = "upload/upload_go";
-
 $route['upload/upload_coin_go'] = "upload/upload_coin_go";
-
 $route['upload/upload_banknote_go'] = "upload/upload_banknote_go";
-
 $route['upload/crop_imgs'] = "upload/crop_imgs";
 
+/* Seccion editar */
 $route['edit/(phonecard|coin|banknote)/(:num)'] = "edit/view/$1/$2";
-
 $route['edit/(phonecard|coin|banknote)/new_image'] = "edit/upload_image/$1/";
-
 $route['edit/(phonecard|coin|banknote)/edit_go'] = "edit/edit_go/$1";
-
 $route['edit/(phonecard|coin|banknote)/crop_imgs'] = "edit/crop_imgs/$1";
 
+/* Seccion Explorar */
+$route['explore'] = "explore/phonecard";
+$route['explore/(phonecard|coin|banknote)'] = "explore/$1";
+$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7";
+$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/no_variations'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/0/1";
+$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/(by_catalog|by_reference|by_face_value|by_serie)'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/$8";
+$route['explore/phonecard/(dated|undated|not-emmited)/(chip|magnetic-band|optical|remote-memory|induced)/([a-z]+)/([a-z0-9]+)/?([0-9]*)/([a-zA-Z0-9]+)/?([0-9]*)/(by_catalog|by_reference|by_face_value|by_serie)/no_variations'] = "explore/explore_phonecards/$1/$2/$3/$4/$5/$6/$7/$8/1";
+$route['explore/coin/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/([a-z0-9]+)/(:num)'] = "explore/explore_coin/$1/$2/$3/$4/$5/$6/$7/$8/$9";
+$route['explore/coin/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/([a-z0-9]+)/(:num)/no_variations'] = "explore/explore_coin/$1/$2/$3/$4/$5/$6/$7/$8/0/1";
+$route['explore/banknote/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/(:num)'] = "explore/explore_banknote/$1/$2/$3/$4/$5/$6/$7/$8";
+$route['explore/banknote/(total|normal|special|other)/([a-z]+)/(:num)/(:num)/(:num)/(:num)/([a-z0-9]+)/(:num)/no_variations'] = "explore/explore_banknote/$1/$2/$3/$4/$5/$6/$7/0/1";
+
+/* Seccion Listados */
+$route['export/(pdf|excel)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)'] = "export/view/$1/$2/$3/$4/$5/$6/$7/$8/$9";
+
+/* Seccion Eventos */
+$route['event/create'] = "event/new_event";
+$route['event/create_go'] = "event/create";
+$route['event/edit/(:num)'] = "event/edit/$1";
+$route['event/edit/edit_go'] = "event/edit_go";
+$route['event/(:num)(-[a-zA-Z0-9]*)?'] = "event/view/$1";
+$route['event/(:num)(-[a-zA-Z0-9]*)?/invite'] = "event/invite/$1";
+$route['event/events_list'] = "event/events_list";
+
+/* Seccion Comercio */
+$route['trade/([A-Za-z0-9]+)'] = "trade/select_only_user/$1";
+$route['trade/(phonecard|coin|banknote)/(:num)'] = "trade/select/$1/$2";
+$route['trade/exchange/([A-Za-z0-9]+)/step_2'] = "trade/exchange_step2/$1";
+$route['trade/(buy|exchange)/([A-Za-z0-9]+)'] = "trade/create_only_user/$1/$2";
+$route['trade/(buy|exchange)/(phonecard|coin|banknote)/(:num)'] = "trade/create/$1/$2/$3";
+$route['trade/(buy|exchange)/(phonecard|coin|banknote)/(:num)/(:num)/([A-Za-z0-9]+)'] = "trade/create/$1/$2/$3/$4/$5";
+$route['trade/trade_buy'] = "trade/trade_buy";
+$route['trade/trade_exchange'] = "trade/trade_exchange";
+$route['trade/trade_details/(:num)'] = "trade/trade_details/$1";
+
+/* Seccion Mi Cuenta */
+$route['account'] = "user/viewAccount";
+
+/* Seccion Comparar */
 $route['compare/([A-Za-z0-9]+)'] = "compare/view/$1";
-
 $route['compare/([A-Za-z0-9]+)/(phonecards|coins|banknotes|stamps)'] = "compare/view/$1/$2";
-
 $route['compare/([A-Za-z0-9]+)/(phonecards|coins|banknotes|stamps)/(wish|swap|buy|sell)'] = "compare/$2/$1/$3";
 
-$route['sendFeedback'] = "user/sendFeedback";
-
-//profile Glass
+/* Seccion Profile Glass */
 $route['profile_glass/(:any)'] = "user/profile_glass/$1";
 
 /* Panel */
-
 $route['answer_feedback'] = "panel/answer_feedback";
 $route['answer_feedback/send'] = "panel/send_feedback";
 
-
+/* Seccion Busqueda */
 $route['search/phonecards/(:any)/(:num)'] = "search/phonecards/$1/$2";
-
 $route['search/coins/(:any)/(:num)'] = "search/coins/$1/$2";
-
 $route['search/(:any)'] = "search/view/$1";
 
+/* Seccion usuario */
+$route['forgot_password'] = "user/forgot_password";
+$route['reset_password/(:num)/(:any)'] = "user/reset_password/$1/$2";
+$route['edit_user'] = "user/edit_user";
+$route['signup'] = "user/new_user";
+$route['signin'] = "user/login";
+$route['activate_user/(:num)/(:any)'] = "user/activate_user/$1/$2";
+$route['sendFeedback'] = "user/sendFeedback";
 $route['(:any)/pdf/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)'] = "user/show_phonecard_collection_pdf/$1/$2/$3/$4/$5/$6/$7/$8";
 $route['(:any)/xls/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)'] = "user/show_phonecard_collection_xls/$1/$2/$3/$4/$5/$6/$7/$8";
-
 $route['(:any)/collection/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)/(:num)'] = "user/show_phonecard_collection/$1/$2/$3/$4/$5/$6/$7/$8";
-
-$route['(:any)'] = "user/view/$1";
-
-
-
+$route['(:any)'] = "user/view/$1"; // ESTA SIEMPRE DEBE IR DE ULTIMO, SIRVE PARA DIRIGIR A LOS USUARIOS A SU PAGINA
 
 
 /* End of file routes.php */
